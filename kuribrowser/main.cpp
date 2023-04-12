1b02753d3b8a4f75b56893a50409f4a8158071a6
Charlie <chazza.phone@gmail.com>
2023-04-12 23:40:40 +0100

    code tidying, buttons added, progress

--- kuribrowser/main.cpp
+++ kuribrowser/main.cpp
@@ -2,58 +2,177 @@
 #include "imgui/backends/imgui_impl_opengl3.h"
 #include "imgui/backends/imgui_impl_glfw.h"
 #include "imgui/imfilebrowser.h"
-#include "rapidjson/filereadstream.h"
 #include <iostream>
+#include <filesystem>
+#include "Windows.h"
+#include <tchar.h>
+#include <shellapi.h>
 #include <fstream>
 #include <string>
-#include <sstream>
 #include <cstdio>
-#define GL_SILENCE_DEPRECATION
+#include "main.h"
 #if defined(IMGUI_IMPL_OPENGL_ES2)
 #include <GLES2/gl2.h>
 #endif
 #include <GLFW/glfw3.h> // Will drag system OpenGL headers
-
-#include "rapidjson/document.h"
-#include "rapidjson/istreamwrapper.h"
-#ifndef RAPIDJSON_DEFAULT_ALLOCATOR
-#define RAPIDJSON_DEFAULT_ALLOCATOR ::RAPIDJSON_NAMESPACE::MemoryPoolAllocator< ::RAPIDJSON_NAMESPACE::CrtAllocator >
-#endif
 #if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
 #pragma comment(lib, "legacy_stdio_definitions.lib")
+#pragma comment(lib, "shell32")
 #endif
 // setup error callback
 static void glfw_error_callback(int error, const char* description)
 {
-    fprintf(stderr, "GLFW Error %d: %s\n", error, description);
+    fprintf(stderr, "GLFW Error %d: %s\n", error, description);  // NOLINT(cert-err33-c)
 }
 
-
-ImGui::FileBrowser InitializeFileMenu()
+ImGui::FileBrowser initialize_file_menu()
 {
     // init file browser
-    ImGui::FileBrowser fileDialog = ImGui::FileBrowser(ImGuiFileBrowserFlags_SelectDirectory);
-    fileDialog.SetTitle("Add a new project");
-    return fileDialog;
+    // ReSharper disable once CppUseAuto
+    file_dialog = ImGui::FileBrowser(ImGuiFileBrowserFlags_SelectDirectory);
+    file_dialog.SetTitle("Add a new project");
+    return file_dialog;
 }
 
-void FileSelected()
+void initialize_table_values()
 {
+    line_count = 0;
+    lines.clear();
+    lines.shrink_to_fit();
+    // FUCK IT WE'RE USING TXT
+    std::ifstream file("filePaths.txt", std::ios::in);
+    std::string line;
+    while (std::getline(file, line))
+    {
+        lines.push_back(line);
+        line_count++;
+    }
+    std::cout << "file line count: " << line_count;
+    file.close();   
+}
+
+bool verify_if_file_path_exists(const std::filesystem::path& path)
+{
+    return std::filesystem::exists(path);
+}
+
+void add_path_to_file(const LPCSTR path)
+{
+    std::ofstream file("filePaths.txt", std::ios::app);
+    file << path << std::endl;
+    file.close();
+    initialize_table_values();
+}
+
+void launch_in_explorer(const LPCSTR path)
+{
+    process = ShellExecuteA(nullptr, "explore", path, nullptr, nullptr, SW_SHOWNORMAL);
+    if (reinterpret_cast<int>(process) <= 32)  // NOLINT(clang-diagnostic-pointer-to-int-cast)
+        std::cout << "error" << std::endl;
     
 }
 
-
-static void ShowFileMenu(ImGui::FileBrowser uiFileBrowser)
+void draw_table(const int line_count)
 {
+    if (ImGui::BeginTable("MainTable", 3, ImGuiTableFlags_Borders))
+    {
+        ImGui::TableSetupColumn("Title");
+        ImGui::TableSetupColumn("Path");
+        ImGui::TableSetupColumn("Type");
+        ImGui::TableHeadersRow();
+        for (int row = 0; row < line_count; row++)
+        {
+            is_selected = (selected_item == row);
+            element = lines[row];
+            ImGui::TableNextRow();
+            ImGui::TableNextColumn();
+            if (ImGui::Selectable((std::to_string(row).c_str()), is_selected, ImGuiSelectableFlags_SpanAllColumns))
+                selected_item = row;
 
+            // ImGui::Text("Path", row);
+            ImGui::TableNextColumn();
+            ImGui::Text(element.c_str());  // NOLINT(clang-diagnostic-format-security)
+            ImGui::TableNextColumn();
+            ImGui::Text("Sus");
+        }
+        ImGui::EndTable();
+    }
 }
 
-int get_current_selection_index(bool *selections)
+void open_folder_button()
 {
-    for (auto current_index = 0; current_index < sizeof(selections); current_index++)
+    if (ImGui::Button("Open Folder"))
+    {
+        std::cout << "\nindex: " << selected_item << std::endl;
+        std::cout << "value: " << lines[selected_item] << std::endl;
+
+        const std::string path = lines[selected_item];
+        launch_in_explorer(path.c_str());
+    } 
+}
+
+void open_project_button()
+{
+    if (ImGui::Button("Open Project"))
+    {
+        const std::string path = lines[selected_item];
+        for (const auto& entry : std::filesystem::recursive_directory_iterator(path))
+        {
+            if (entry.is_regular_file() && entry.path().extension() == ".sln")
+            {
+                sln_path = entry.path().string();
+                vs_cmd = vs_path + " \"" + sln_path + R"( " /command "File.OpenSolution")";
+                std::cout << sln_path << std::endl;
+                std::cout << vs_cmd << std::endl;
+                process = ShellExecuteA(nullptr, "open", vs_path.c_str(), sln_path.c_str(), nullptr, SW_SHOWNORMAL);
+                if (reinterpret_cast<int>(process) <= 32)
+                {
+                    std::cout << "error opening sln";
+                }
+            }   
+        }
+    }
+}
+
+void open_unity_button()
+{
+    if (ImGui::Button("Open in Unity"))
+    {
+        
+    }
+}
+
+void open_unreal_button()
+{
+    if (ImGui::Button("Open in Unreal"))
+    {
+        
+    }
+}
+
+int get_current_selection_index(std::vector<bool> selections)
+{
+    for (auto current_index = 0; current_index < static_cast<int>(selections.size()); current_index++)
         if (selections[current_index])
             return current_index;
     return -1;
+}
+
+void draw_menu_bar()
+{
+    // create menu bar
+    if (ImGui::BeginMainMenuBar())
+    {
+        if (ImGui::BeginMenu("File"))
+        {
+            if (ImGui::MenuItem("Add New Project"))
+            {
+                file_browser.Open();
+            }
+            ImGui::EndMenu();
+        }
+        ImGui::EndMainMenuBar();
+    }
 }
 
 
@@ -69,11 +188,11 @@ void main(int, char**)
 
     
     // setup renderer and window
-    const char* glsl_version = "#version 130";
+    glsl_version = "#version 130";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
     
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "Main Window", NULL, NULL);
+    window = glfwCreateWindow(1280, 720, "Main Window", NULL, NULL);
     if (window == nullptr)
         return;
     glfwMakeContextCurrent(window);
@@ -97,79 +216,21 @@ void main(int, char**)
     ImGui_ImplOpenGL3_Init(glsl_version);
 
     // tell what windows to display
-    bool bShowMainWindow = true;
+    bShowMainWindow = true;
 
     // make window maximised
-    const ImGuiViewport* viewport = ImGui::GetMainViewport();
+    viewport = ImGui::GetMainViewport();
     ImGui::SetNextWindowPos(viewport->WorkPos);
     ImGui::SetNextWindowSize(viewport->WorkSize);
     ImGui::SetNextWindowViewport(viewport->ID);
 
     // init file browser
-    auto file_browser = InitializeFileMenu();
+    file_browser = initialize_file_menu();
 
 
 
-    // FUCK IT WE'RE USING TXT
-    std::ifstream file("filePaths.txt", std::ios::in);
-    std::vector<std::string> lines;
-    std::string line;
-    auto line_count = 0;
-    while (std::getline(file, line))
-    {
-        lines.push_back(line);
-        line_count++;
-    }
-    std::cout << "file line count: " << line_count;
+    initialize_table_values();
 
-    //
-    // std::ofstream newFile("newFile2.txt");
-    // std::cout << "Among\n Us";
-    // init local projects
-
-    // JSON PARSING
-    // std::ifstream file("filePaths.json");
-    // std::ostringstream tmp;
-    // tmp << file.rdbuf();
-    // std::string s = tmp.str();
-    //
-    // rapidjson::Document d;
-    // d.Parse<0>(s.c_str());
-    //
-    // std::cout << d["Test"].GetType();
-
-
-    //FILE* fp = fopen("filePaths.json", "rb");
-    //char read_buffer[65536];
-    //rapidjson::FileReadStream is(fp, read_buffer, sizeof(read_buffer));
-    //rapidjson::Document d;
-    //d.ParseStream(is);
-    //if (d.HasParseError())
-    //{
-    //    std::cout << d.GetParseError();
-    //    
-    //}
-    //fclose(fp);
-    // std::ifstream file_stream("filePaths.json");
-    // rapidjson::IStreamWrapper isw(file_stream);
-    // rapidjson::Document d;
-    // d.ParseStream(file_stream);
-    // if (d.HasParseError())
-    // {
-    //     std::cout << d.GetParseError();
-    // }
-    // std::cout << d["Test"].GetString();
-    // for (const auto& point : d["ProjectPaths"].GetArray())
-    // {
-    //     std::cout << "hello";
-    // }
-    
-    // std::cout << std::string_view(doc["ProjectPaths"]) << std::endl;
-    // for (simdjson::ondemand::object path : foundPaths)
-    // {
-    //     std::cout << "File Path: " << simdjson::to_json_string(path) << std::endl;
-    // }
-    
     // MAIN PROGRAM
     while (!glfwWindowShouldClose(window))
     {
@@ -182,67 +243,28 @@ void main(int, char**)
 
         if (bShowMainWindow)
         {
-            // create menu bar
-            if (ImGui::BeginMainMenuBar())
-            {
-                if (ImGui::BeginMenu("File"))
-                {
-                    if (ImGui::MenuItem("Add New Project"))
-                    {
-                        file_browser.Open();
-                    }
-                    ImGui::EndMenu();
-                }
-                ImGui::EndMainMenuBar();
-            }
-            
+
+            draw_menu_bar();
             // create main window 
             ImGui::Begin("Main Window");
-            if (ImGui::BeginTable("MainTable", 3, ImGuiTableFlags_Borders))
-            {
-
-                static bool selected[4] = {};
-                ImGui::TableSetupColumn("Title");
-                ImGui::TableSetupColumn("Path");
-                ImGui::TableSetupColumn("Type");
-                ImGui::TableHeadersRow();
-                for (int row = 0; row < line_count; row++)
-                {
-                    std::string& element = lines[row];
-                    ImGui::TableNextRow();
-                    ImGui::TableNextColumn();
-                    if (ImGui::Selectable((std::to_string(row).c_str()), &selected[row], ImGuiSelectableFlags_SpanAllColumns))
-                    {
-                        for (auto &field : selected)
-                            field = false;
-                        
-                        selected[row] = true;
-                    }
-
-                    // ImGui::Text("Path", row);
-                    ImGui::TableNextColumn();
-                    ImGui::Text(element.c_str());
-                    ImGui::TableNextColumn();
-                    ImGui::Text("Sus");
-                }
-
-                ImGui::EndTable();
-                if (ImGui::Button("Open"))
-                    std::cout << lines[get_current_selection_index(selected)] << std::endl;
-            }
             
-            // auto selected = file_browser.GetSelected().string();
-            // char arr[800];
-            // strcpy(arr, selected.c_str());
-            // ImGui::Text(arr);
-            //
+            draw_table(line_count);
+            open_folder_button();
+            open_project_button();
+            open_unity_button();
+            open_unreal_button();
+            
             ImGui::End();
             file_browser.Display();
 
-            // if(file_browser.HasSelected())
-            // {
-            //     selected = file_browser.GetSelected().string();
-            // }
+            if (file_browser.HasSelected())
+            {
+                selected_path = file_browser.GetSelected().string();
+                if (!verify_if_file_path_exists(selected_path))
+                    return;
+                add_path_to_file(selected_path.c_str());
+                file_browser.ClearSelected();
+            }
         }
         // OUTSIDE MAIN WINDOW
 
---
+++ kuribrowser/main.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+// exe paths
+inline std::string vs_path = "D:\\VS2022\\Common7\\IDE\\devenv.exe";
+inline std::string vs_cmd;
+// sln paths
+inline std::string sln_path;
+
+// glfw initialization defs
+inline const char* glsl_version;
+inline GLFWwindow* window;
+
+// imgui initialization defs
+inline ImGuiViewport* viewport;
+inline bool bShowMainWindow;
+
+// table values
+inline int line_count = 0;
+
+// table selection
+inline std::string element;
+inline bool is_selected;
+inline static int selected_item = -1;
+inline std::vector<std::string> lines;
+
+// file browser
+inline ImGui::FileBrowser file_dialog;
+inline ImGui::FileBrowser file_browser;
+inline std::string selected_path;
+// process management
+inline HINSTANCE process;
